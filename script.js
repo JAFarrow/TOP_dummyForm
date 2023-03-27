eabae0c3322a1b777dad75ef86097336fea220f8
JAFarrow <f.justin@tutanota.com>
2023-03-27 22:17:38 +0200

    Added Footer. Footer implemented.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const pswrd = document.getElementById('pswrd');
 const pswrdVerif = document.getElementById('pswrdVerif');
 
 pswrdVerif.addEventListener('input', () => {
-  if (pswrdVerif.value != pswrd.value) {
+  if (pswrdVerif.value != pswrd.value && pswrdVerif.value != '') {
     pswrd.classList.add('pswrdInvalid');
     pswrdVerif.classList.add('pswrdInvalid');
   } else {
