4d3fb63c743e2431d96eeb5c1c84851f4e3bb121
JAFarrow <f.justin@tutanota.com>
2023-03-27 21:59:30 +0200

    Form Validation Implemented basic HTML form validation along with js verification of matching password inputs.

---
+++ script.js
@@ -0,0 +1,12 @@
+const pswrd = document.getElementById('pswrd');
+const pswrdVerif = document.getElementById('pswrdVerif');
+
+pswrdVerif.addEventListener('input', () => {
+  if (pswrdVerif.value != pswrd.value) {
+    pswrd.classList.add('pswrdInvalid');
+    pswrdVerif.classList.add('pswrdInvalid');
+  } else {
+    pswrd.classList.remove('pswrdInvalid');
+    pswrdVerif.classList.remove('pswrdInvalid');
+  }
+});
